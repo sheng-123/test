8c5c7cdd077415431b54210635f70eabab69f407
花满楼 <ssd614993272@qq.com>
2021-05-18 06:59:07 +0800

    版本3

--- src/main/java/com/sheng/Test01.java
+++ src/main/java/com/sheng/Test01.java
@@ -1,9 +1,17 @@
 package com.sheng;
 
+import org.junit.Test;
+
 /**
  * @author: sheng
  * @create: 2021/5/17 21:57
  * @description: heh
  **/
 public class Test01 {
+    @Test
+    public  void test(){
+        System.out.println("版本3");
+        System.out.println("版本4");
+        System.out.println("版本5");
+    }
 }
