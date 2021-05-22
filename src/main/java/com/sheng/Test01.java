3bf97cee216635b5b0a8f1fcb3c08d86e74c17ce
花满楼 <ssd614993272@qq.com>
2021-05-22 20:56:30 +0800

    主干提交

--- src/main/java/com/sheng/Test01.java
+++ src/main/java/com/sheng/Test01.java
@@ -1,9 +1,16 @@
 package com.sheng;
 
+import org.junit.Test;
+
 /**
  * @author: sheng
  * @create: 2021/5/17 21:57
  * @description: heh
  **/
 public class Test01 {
+    @Test
+    public  void test(){
+        System.out.println("这是主干");
+    }
+
 }
